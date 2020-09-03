ddd1b49b042dea1aced08aa148a3281ffa874c19
Bryan Lim <bryan.lim@duke.edu>
2020-09-03 15:58:24 -0400

    Working code format for line_predictor_module

--- line_predictor_module.py
+++ line_predictor_module.py
@@ -10,10 +10,10 @@ def find_gradient(a, b):
     x2 = b[0]
 
     y1 = a[1]
-    y2 = b[2]
+    y2 = b[1]
     
     # find the gradient
-    m = (y2 - y1)/(x2-x1) # Rise over run
+    m = (y2 - y1)/(x2 - x1) # Rise over run
     return m
 
 def line_function(a, b, x3):
@@ -24,5 +24,11 @@ def line_function(a, b, x3):
     # do predictor by the typical y = mx
     
     y3 = gradient_m * x3
+    return y3
     
+    
+# The following code just helps me manually test at command line that my function works
+if __name__ == "__main__":
+    final_answer = line_function((1, 2), (10, 20), 100)
+    print(final_answer)
     
